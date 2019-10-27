23125dd07dcf98de1255cebc4805df6f54393f71
Celine Seghbossian <celineblaine@icloud.com>
2019-10-27 06:00:05 -0700

    more debugging

--- prog2/prog2.js
+++ prog2/prog2.js
@@ -43,6 +43,9 @@ function main() {
   generateCylinderData();
   generateTreeData();
 
+  console.log("cylinderLines", cylinderLines);
+  console.log("cylinderVerts", cylinderVerts);
+  console.log("cylinderTriangles", cylinderTriangles);
   reload();
 }
 
@@ -198,18 +201,19 @@ function dodecagons() {
   var d = [];
   //base
   for(i = 0; i < 12; i++) {
-    var x = r*Math.cos(i*(2*Math.PI/12));
-    var y = r*Math.sin(i*(2*Math.PI/12));
+    var x = Math.cos(i*(2*Math.PI/12));
+    var y = Math.sin(i*(2*Math.PI/12));
     var z = 0 ;
     d.push(x,y,z);
   }
   //top
   for(i = 0; i < 12; i++) {
-    var x = 0.5*r*Math.cos(i*(2*Math.PI/12));
-    var y = 0.5*r*Math.sin(i*(2*Math.PI/12));
+    var x = 0.5*Math.cos(i*(2*Math.PI/12));
+    var y = 0.5*Math.sin(i*(2*Math.PI/12));
     var z = 10;
     d.push(x,y,z);
   }
+  console.log("in dodecagons d=", d);
   return d;
 }
 
@@ -346,9 +350,11 @@ function generateTreeData() {
 function generateCylinderData() {
   //generate cylinder data
   var ve = dodecagons();
+  console.log("ve = ", ve); 
   var nn = findNormSegments(ve);
   ve = ve.concat(nn);
 
+  console.log("ve = ", ve);
   //index 0-71 are dodecagon vertices
   //index 72-143 are normal end points
   cylinderVerts = new Float32Array(ve);
